c9638817a2ec48276817b79fd77f772f62f234b4
CAGATAY DINC <cagatay210@hotmail.com>
2025-11-03 22:10:15 +0300

    new update2

--- pages/base_page2.py
+++ pages/base_page2.py
@@ -1,11 +1,27 @@
 from playwright.sync_api import Page, expect
-
+#Önce locatorler tanımlanır daha sonra yapacakları işlemler
 class Login_Isnet:
     def __init__(self, page:Page):
         self.page = page
         self.destek_button = page.get_by_role("link", name="Destek")
         self.ad_input = page.locator('input[placeholder="Adınız"]')
         self.soyad_input = page.locator('input[placeholder="Soyadınız"]')
+        self.sirketadi_input = page.get_by_role("textbox", name="Şirket Adı")
+        self.phoneNumber_input = page.get_by_role("textbox", name="Telefon")
+        self.email_input = page.get_by_role("textbox", name="E-posta")
+        self.aciklama_input = page.get_by_role("textbox", name="Mesajınız")
+        self.kvkk_checkbox = page.get_by_role("checkbox", name="Kişisel Verilerin Korunması")
+        self.kvkk2_checkbox =  page.get_by_role("checkbox", name="Elektronik Ticari İleti Açık")
+       #self.captcha_checkbox = page.locator("iframe[name=\"a-49wdudm94mpq\"]").content_frame.get_by_role("checkbox", name="Ben robot değilim")
+        self.gonder_button = page.get_by_role("button", name="Gönder")
+
+
+
+
+
+
+
+
 
 
     def goto(self):
@@ -22,4 +38,26 @@ class Login_Isnet:
 
         self.soyad_input.fill('DİNCTEST')
 
+    def fill_sirketadi(self):
+        self.sirketadi_input.fill("TESTFİRMA")
 
+    def fill_phoneNumber(self):
+        self.phoneNumber_input.fill('5324645159')
+
+    def fill_email(self):
+        self.email_input.fill("test@gmail.com")
+
+    def fill_aciklama(self):
+        self.aciklama_input.fill("TEST AMAÇLIDIR DİKKATE ALMAYINIZ")
+
+    def checkbox_kvk(self):
+        self.kvkk_checkbox.click()
+
+    def checkbox_kvkk2(self):
+        self.kvkk2_checkbox.click()
+
+   #def checkbox_captcha(self): -- CAPTCHA KONTROLU BYPASS EDİLMELİ
+    #   self.captcha_checkbox.click()
+
+    def button_gonderme(self):
+        self.gonder_button.click()
--- tests/ui/test_2.py
+++ tests/ui/test_2.py
@@ -8,5 +8,13 @@ def test_example(page: Page):
     login_page.click_button()
     login_page.fill_ad()
     login_page.fill_soyad()
+    login_page.fill_sirketadi()
+    login_page.fill_phoneNumber()
+    login_page.fill_email()
+    login_page.fill_aciklama()
+    login_page.checkbox_kvk()
+    login_page.checkbox_kvkk2()
+    #login_page.checkbox_captcha()  -- CAPTCHA KONTROLU BYPASS EDİLMELİ
+    login_page.button_gonderme()
     page.wait_for_timeout(10000)
 

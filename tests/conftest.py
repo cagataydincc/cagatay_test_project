97900d29e2122efd7eadd30093b4a90cb0f5c738
CAGATAY DINC <cagatay210@hotmail.com>
2025-11-21 12:04:28 +0300

    new update4

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,14 @@
+import sys
+import os
 import pytest
 from playwright.sync_api import sync_playwright
+
+# --------------------------------------------------
+# Proje kök dizinini Python path'e ekle
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(PROJECT_ROOT)
+sys.path.append(os.path.abspath(os.path.join(PROJECT_ROOT, "..")))  # bir üst klasör
+# --------------------------------------------------
 
 @pytest.fixture(scope="session")
 def browser():
